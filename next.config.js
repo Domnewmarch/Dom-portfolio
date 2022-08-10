c2a5106ad92541036eee33beda73ffad19843ada
Domnewmarch <dom.newmarch@parall.ax>
2022-08-10 13:30:36 +0100

    add purgecss

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
-const path = require("path");
+const path = require('path')
+const withCss = require('@zeit/next-css')
+const withPurgeCss = require('next-purgecss')
+
+module.exports = withCss(withPurgeCss())
 
 module.exports = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "css")],
+    includePaths: [path.join(__dirname, 'css')]
   },
   trailingSlash: true,
   devIndicators: {
-    buildActivity: false,
+    buildActivity: false
   },
   eslint: {
-    ignoreDuringBuilds: false,
-  },
-};
+    ignoreDuringBuilds: false
+  }
+}
